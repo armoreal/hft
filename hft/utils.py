a3318e114daaca77a1425623e3e6e766426e2811
Jingwei Wu <wazai7@gmail.com>
2017-06-19 21:26:17 -0400

    rename a column

--- hft/utils.py
+++ hft/utils.py
@@ -9,6 +9,6 @@ Utility functions
 def aggregate(pxall, group, funs, rename_dict=None):
     daily_agg_px = pxall.groupby(['date', group]).agg(funs)
     daily_agg_px.rename(columns=rename_dict, inplace=True)
-    daily_agg_px['count'] = pxall.groupby(['date', group]).size()
+    daily_agg_px['n_trades'] = pxall.groupby(['date', group]).size()
     agg_px = daily_agg_px.reset_index().groupby(group).median()
     return agg_px
