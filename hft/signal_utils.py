d123b20b2a51a9981f90a2cc7ed72cd2129c060d
Jingwei Wu <wazai7@gmail.com>
2017-06-24 16:21:41 -0400

    compute signals and returns in a generic way

--- hft/signal_utils.py
+++ hft/signal_utils.py
@@ -8,74 +8,85 @@ import logging
 import numpy as np
 import pandas as pd
 
+import hft.utils as utils
+
 logger = logging.getLogger(__name__)
 
 with open(os.path.join('config', 'signal.json')) as signal_config_file:
     signal_config = json.load(signal_config_file)
-N_FLEXIBLE_HALF_SECONDS = signal_config['n_flexible_half_seconds']
+# N_FLEXIBLE_HALF_SECONDS = signal_config['n_flexible_half_seconds']
 
 
-def order_imbalance_ratio(px):
+def order_imbalance_ratio(px, backward_seconds, forward_seconds):
+    """
+    Order Imbalance Ratio (Shen, 2015)
+    """
     px['order_imbalance_ratio'] = (px['b1_size']-px['s1_size']) / (px['b1_size']+px['s1_size'])
+    px = utils.moving_operate(px, 'order_imbalance_ratio', np.mean, backward_seconds, forward_seconds)
     return px
 
 
-def volume_order_imbalance(px):
-    px['delta_b1'] = 0.0
-    idx = px.b1 == px.b1.shift(1)
-    px.loc[idx, 'delta_b1'] = px['b1_size'][idx] - px['b1_size'].shift(1)[idx]
-    idx = px.b1 > px.b1.shift(1)
-    px.loc[idx, 'delta_b1'] = px['b1_size'][idx]
-    px.set_value(px.index[0], 'delta_b1', np.nan)
+def get_order_imbalance_column_name(conservative):
+    return ('conservative_' if conservative else '') + 'order_flow_imbalance'
 
-    px['delta_s1'] = 0.0
-    idx = px.s1 == px.s1.shift(1)
-    px.loc[idx, 'delta_s1'] = px['s1_size'][idx] - px['s1_size'].shift(1)[idx]
-    idx = px.s1 < px.s1.shift(1)
-    px.loc[idx, 'delta_s1'] = px['s1_size'][idx]
-    px.set_value(px.index[0], 'delta_s1', np.nan)
 
-    px['volume_order_imbalance'] = px['delta_b1'] - px['delta_s1']
+def single_order_imbalance(px, conservative=False):
+    """Order Flow Imbalance
+     Reference: [1] Rama Cont, Kukanov and Stoikov (2011)
+                [2] D Shen (2015)
+
+    :param px: pandas data frame
+    :param conservative: logical, if True use definition in [1], otherwise use definition in [2]
+    :return: pandas data frame with OFI column appended
+    """
+    px['delta_b1'] = (px.b1 >= px.b1.shift(1)) * px.b1_size - (px.b1 <= px.b1.shift(1)) * px.b1_size.shift(1)
+    px['delta_s1'] = (px.s1 <= px.s1.shift(1)) * px.s1_size - (px.s1 >= px.s1.shift(1)) * px.s1_size.shift(1)
+    if conservative:
+        px.loc[px.b1 < px.b1.shift(1), 'delta_b1'] = 0.0
+        px.loc[px.s1 > px.s1.shift(1), 'delta_s1'] = 0.0
+    col_name = get_order_imbalance_column_name(conservative)
+    px[col_name] = px['delta_b1'] - px['delta_s1']
     px.drop(['delta_b1', 'delta_s1'], axis=1, inplace=True)
     return px
 
 
-def period_return(px, seconds):
-    """Compute the return for every point within a fixed time period going forward or backward
-
-    :param px: pandas data frame, with column return
-    :param seconds: integer, could be positive (forward) or negative (backward)
-    :return: pandas data frame, augmented by period return
-    """
-    shift_second_col_name = 'second'+str(seconds)
-    px[shift_second_col_name] = px['second'] + seconds
-
-    # there are many missing shifted mids (approximately 50%) so we allow flexible period
-    px['orig_shift_second'] = px[shift_second_col_name]
-    for i in range(N_FLEXIBLE_HALF_SECONDS):
-        px.loc[~px[shift_second_col_name].isin(px.second), shift_second_col_name] = \
-            px.loc[~px[shift_second_col_name].isin(px.second), 'orig_shift_second'] + 0.5*(i+1)
-        px.loc[~px[shift_second_col_name].isin(px.second), shift_second_col_name] = \
-            px.loc[~px[shift_second_col_name].isin(px.second), 'orig_shift_second'] - 0.5*(i+1)
-    px.drop('orig_shift_second', axis=1, inplace=True)
-
-    px_copy = px[['second', 'mid']].copy()
-    shift_mid_col_name = 'mid'+str(seconds)
-    px_copy.columns = [shift_second_col_name, shift_mid_col_name]
-    px = pd.merge(px, px_copy, on=shift_second_col_name, how='left')
-    shift_return_col_name = 'return'+str(seconds)
-    if seconds > 0:
-        px['price_change'] = px[shift_mid_col_name] - px.mid
-        px[shift_return_col_name] = px['price_change'] / px.mid
-    else:
-        px['price_change'] = px.mid - px[shift_mid_col_name]
-        px[shift_return_col_name] = px['price_change'] / px[shift_mid_col_name]
+def order_flow_imbalance(px, backward_seconds, forward_seconds, conservative=False):
+    if 'order_flow_imbalance' not in px.columns:
+        px = single_order_imbalance(px, conservative)
+    col_name = get_order_imbalance_column_name(conservative)
+    px = utils.moving_operate(px, col_name, sum, backward_seconds, forward_seconds)
     return px
 
 
-def signal_on_multiple_dates(pxall, fun):
-    dates = set(pxall.date)
+def period_return(price_series):
+    price_array = np.array(price_series)
+    return np.nan if len(price_array) == 1 else (price_array[-1] - price_array[0]) / price_array[0]
+
+
+def period_tick_move(price_series, tick_size):
+    price_array = np.array(price_series)
+    return np.nan if len(price_array) == 1 else (price_array[-1] - price_array[0]) / tick_size
+
+
+def period_mid_move(px, backward_seconds, forward_seconds, tick_size):
+    """
+    Compute period price move, price tick move and return
+    """
+    px = utils.moving_operate(px, 'mid', lambda x: period_tick_move(x, tick_size),
+                              backward_seconds, forward_seconds, 'tick_move')
+    px = utils.moving_operate(px, 'mid', period_return, backward_seconds, forward_seconds, 'return')
+    return px
+
+
+def signal_on_multiple_dates(pxall, signal):
+    """Compute signal over multiple days
+
+    :param pxall: pandas data frame, price data
+    :param signal: function to compute one signal
+    :return: pandas data frame with signal column appended
+    """
+    dates = sorted(list(set(pxall.date)))
     logger.info('Computing signal from %s to %s', dates[0], dates[-1])
-    px_list = [fun(pxall[pxall.date == x].copy()) for x in dates]
+    px_list = [signal(pxall[pxall.date == x].copy()) for x in dates]
     px_enrich = pd.concat(px_list)
     return px_enrich
--- tests/test_signal_utils.py
+++ tests/test_signal_utils.py
@@ -6,12 +6,14 @@ import hft.signal_utils as signal
 logging.basicConfig(level=logging.INFO, format='%(asctime)s  %(name)s  %(levelname)s  %(message)s')
 
 product = 'cu'  # switch between cu and zn
+tick_size = 10 if product == 'cu' else 5
 seconds = 10
 return_col = 'return'+str(seconds)
 return_cutoff = 6
 
 # test on a single date
 px = dl.load_active_contract(product, '20131231')
-px = signal.volume_order_imbalance(px)
-px = signal.order_imbalance_ratio(px)
-px = signal.period_return(px, 5)
+px = signal.order_flow_imbalance(px, 1, 0)
+px = signal.order_imbalance_ratio(px, 1, 0)
+px = signal.period_mid_move(px, 5, 0, tick_size)
+px = signal.period_mid_move(px, 0, 10, tick_size)
