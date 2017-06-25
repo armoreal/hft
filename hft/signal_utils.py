616dacc69e2cd6459cf7a777fc7415cfa8b56e50
Jingwei Wu <wazai7@gmail.com>
2017-06-25 17:11:30 -0400

    adapt changes in utils

--- hft/signal_utils.py
+++ hft/signal_utils.py
@@ -17,12 +17,12 @@ with open(os.path.join('config', 'signal.json')) as signal_config_file:
 # N_FLEXIBLE_HALF_SECONDS = signal_config['n_flexible_half_seconds']
 
 
-def order_imbalance_ratio(px, backward_seconds, forward_seconds):
-    """
-    Order Imbalance Ratio (Shen, 2015)
+def order_imbalance_ratio(px, backward_seconds, forward_seconds, index_series):
+    """Order Imbalance Ratio
+    Reference: Carteaa, Donnellyb and Jaimungal (2015)
     """
     px['order_imbalance_ratio'] = (px['b1_size']-px['s1_size']) / (px['b1_size']+px['s1_size'])
-    px = utils.moving_operate(px, 'order_imbalance_ratio', np.mean, backward_seconds, forward_seconds)
+    px = utils.moving_operate(px, 'order_imbalance_ratio', np.mean, backward_seconds, forward_seconds, index_series)
     return px
 
 
@@ -50,11 +50,11 @@ def single_order_imbalance(px, conservative=False):
     return px
 
 
-def order_flow_imbalance(px, backward_seconds, forward_seconds, conservative=False):
-    if 'order_flow_imbalance' not in px.columns:
-        px = single_order_imbalance(px, conservative)
+def order_flow_imbalance(px, backward_seconds, forward_seconds, index_series, conservative=False):
     col_name = get_order_imbalance_column_name(conservative)
-    px = utils.moving_operate(px, col_name, sum, backward_seconds, forward_seconds)
+    if col_name not in px.columns:
+        px = single_order_imbalance(px, conservative)
+    px = utils.moving_operate(px, col_name, sum, backward_seconds, forward_seconds, index_series)
     return px
 
 
@@ -68,25 +68,25 @@ def period_tick_move(price_series, tick_size):
     return np.nan if len(price_array) == 1 else (price_array[-1] - price_array[0]) / tick_size
 
 
-def period_mid_move(px, backward_seconds, forward_seconds, tick_size):
+def period_mid_move(px, backward_seconds, forward_seconds, tick_size, index_series):
     """
     Compute period price move, price tick move and return
     """
     px = utils.moving_operate(px, 'mid', lambda x: period_tick_move(x, tick_size),
-                              backward_seconds, forward_seconds, 'tick_move')
-    px = utils.moving_operate(px, 'mid', period_return, backward_seconds, forward_seconds, 'return')
+                              backward_seconds, forward_seconds, index_series, 'tick_move')
+    px = utils.moving_operate(px, 'mid', period_return, backward_seconds, forward_seconds, index_series, 'return')
     return px
 
 
-def signal_on_multiple_dates(pxall, signal):
+def signal_on_multiple_dates(pxall, func):
     """Compute signal over multiple days
 
     :param pxall: pandas data frame, price data
-    :param signal: function to compute one signal
+    :param func: function to compute one signal
     :return: pandas data frame with signal column appended
     """
     dates = sorted(list(set(pxall.date)))
     logger.info('Computing signal from %s to %s', dates[0], dates[-1])
-    px_list = [signal(pxall[pxall.date == x].copy()) for x in dates]
+    px_list = [func(pxall[pxall.date == x].copy()) for x in dates]
     px_enrich = pd.concat(px_list)
     return px_enrich
