48f779a4c219e0b8cc441bf292024f7349695dd3
Jingwei Wu <wazai7@gmail.com>
2017-06-29 00:29:40 -0400

    add index_folder

--- research/signal_cache.py
+++ research/signal_cache.py
@@ -10,6 +10,7 @@ import hft.signal_utils as signal
 logging.basicConfig(level=logging.INFO, format='%(asctime)s  %(name)s  %(levelname)s  %(message)s')
 
 data_path = os.path.join(os.environ['HOME'], 'dropbox', 'hft', 'data')
+index_folder = os.path.join(data_path, 'index')
 
 # load raw data
 # -------------
@@ -23,33 +24,34 @@ tick_size = ticksize_json[product]
 # dates = dl.get_dates()
 # pxall = dl.load_active_contract_multiple_dates(product, dates)
 # pxall.to_pickle(os.path.join(os.environ['HOME'], 'hft', product+'.pkl'))
-pxall = pd.read_pickle(os.path.join(os.environ['HOME'], 'hft', product+'.pkl'))
+pxall = pd.read_pickle(os.path.join(data_path, product+'_20.pkl'))
 
 # cache index
 # -----------
 
 px = pxall.copy().reset_index()
-second_list = [1, 2, 5, 10, 20]
+# second_list = [1, 2, 5, 10, 20]
+second_list = [30, 60, 120, 180, 300]
 
 for sec in second_list:
     print('----------- sec = ' + str(sec) + ' -------------')
     backward_index = utils.get_index_multiple_dates(px, sec, 0)
-    file_name = os.path.join(data_path, product+'_index_'+str(sec)+'_0.pkl')
+    file_name = os.path.join(index_folder, product+'_index_'+str(sec)+'_0.pkl')
     print('Saving backward index to ' + file_name)
     backward_index.to_pickle(file_name)
 
     forward_index = utils.get_index_multiple_dates(px, 0, sec)
-    file_name = os.path.join(data_path, product+'_index_0_'+str(sec)+'.pkl')
+    file_name = os.path.join(index_folder, product+'_index_0_'+str(sec)+'.pkl')
     print('Saving forward index to ' + file_name)
     forward_index.to_pickle(file_name)
 
 # compute signal
 # --------------
 
-index_folder = os.path.join(data_path, 'index')
 # px = pxall[pxall.date.isin(['2013-10-08', '2013-10-09'])].copy().reset_index()
 px = pxall.copy().reset_index()
-second_list = [1, 2, 5, 10, 20]
+# second_list = [1, 2, 5, 10, 20]
+second_list = [30, 60, 120, 180, 300]
 
 for sec in second_list:
     print('----------- sec = ' + str(sec) + ' -------------')
@@ -71,4 +73,3 @@ for sec in second_list:
     filename = os.path.join(data_path, product+'_'+str(sec)+'.pkl')
     print('Saving forward index to ' + filename)
     px.to_pickle(filename)
-
